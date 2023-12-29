2ab4dc18f0e11da6758c1c0b0952486f410bc7a6
omarmouslam <abomoslamomar@gmail.com>
2023-12-29 15:04:27 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from "express"
 import initApp from "./src/index.router.js";
 import dotenv from "dotenv"
 import cors from "cors"
-dotenv.config()
+import path from "path"
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: path.join(__dirname, '.env') })
 const app= express();
 const port= process.env.PORT || 5000
 
